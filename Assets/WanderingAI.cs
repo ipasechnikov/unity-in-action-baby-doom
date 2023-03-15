b22bffdd45b2548845f32d6e664746ecf2ffbc2c
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-03-15 17:35:38 +0900

    Add WanderingAI component to make enemy wander randomly

---
+++ Assets/WanderingAI.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderingAI : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public float obstracleRange = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var deltaZ = speed * Time.deltaTime;
+        transform.Translate(0, 0, deltaZ);
+
+        // A ray at the same position and pointing in the same direction at the character
+        var ray = new Ray(transform.position, transform.forward);
+
+        if (Physics.SphereCast(ray, 0.75f, out var hit))
+        {
+            if (hit.distance < obstracleRange)
+            {
+                var angle = Random.Range(-110, 110);
+                transform.Rotate(0, angle, 0);
+            }
+        }
+    }
+}
