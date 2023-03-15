da1ee0d289423a6d521f7f2007d5340ee89b1609
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-03-15 23:32:56 +0900

    Add SceneController component to auto spawn enemies

--- Assets/WanderingAI.cs
+++ Assets/WanderingAI.cs
@@ -15,7 +15,7 @@ public class WanderingAI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        IsAlive = false;
+        IsAlive = true;
     }
 
     // Update is called once per frame
