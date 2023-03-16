e622ed5577fa16861b2d9e365d78889f916defc2
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-03-16 12:31:22 +0900

    Make Fireball react to collisions

--- Assets/Fireball.cs
+++ Assets/Fireball.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Fireball : MonoBehaviour
 {
+    [SerializeField] float speed = 10.0f;
+    [SerializeField] int damage = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,16 @@ public class Fireball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        var deltaZ = speed * Time.deltaTime;
+        transform.Translate(0, 0, deltaZ);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        var player = other.GetComponent<PlayerCharacter>();
+        if (player != null)
+            Debug.Log("Player hit");
+
+        Destroy(gameObject);
     }
 }
